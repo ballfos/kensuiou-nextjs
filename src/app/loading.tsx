7841746bbff014524269c0f02f98871aa3ee4c38
Gaku Takahashi <gakut2002@gmail.com>
2025-06-15 11:10:12 +0900

    set a loading aquatan icon

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function HomeLoading() {
+    return (
+    <div className="flex justify-center" aria-label="Now Loading...">
+      <img src="chinups.GIF" alt="Now Loading..."/>
+    </div>
+    );
+  }
